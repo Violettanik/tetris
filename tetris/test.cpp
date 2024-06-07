e7b8e7a8ad1cae68ac25a808610304e5284f44be
Violettanik <146526302+Violettanik@users.noreply.github.com>
2024-06-07 03:14:18 +0300

    Update test.cpp

--- tetris/test.cpp
+++ tetris/test.cpp
@@ -309,28 +309,3 @@ TEST_CASE("field test canNotTurn 2")
     field.initField();
     REQUIRE(field.canNotTurn(fig.getPtrOnPoint()) == 1);
 }
-
-/*
-TEST_CASE("can iterate not full")
-{
-    tree t{{{1, 2, nullptr}, 3, {nullptr, 4, 5}}};
-    int v = 1;
-    for (int i : t) {
-        CAPTURE(i);
-        CHECK(i == v++);
-    }
-    REQUIRE(v == 6);
-}
-
-TEST_CASE("can reverse iterate")
-{
-    tree t{{{1, 2, 3}, 4, {5, 6, 7}}};
-    int v = 7;
-    std::for_each(std::make_reverse_iterator(t.end()),
-                  std::make_reverse_iterator(t.begin()), [&v](int i) {
-                      CAPTURE(i);
-                      CHECK(i == v--);
-                  });
-    REQUIRE(v == 0);
-}
-*/
